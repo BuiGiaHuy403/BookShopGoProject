8ea13d6c8eba76acb46afd45785e368d06a639e0
buigiahuy403 <buigiahuy403@gmail.com>
2025-01-21 14:38:34 +0700

    modify jenkins file

--- internal/testhelpers/connectDB.go
+++ internal/testhelpers/connectDB.go
@@ -28,17 +28,17 @@ func ConnectDBFailed() *gorm.DB {
 	var (
 		host     = "localhost"
 		user     = "postgres"
-		password = "1235"
+		password = "1235" // Incorrect password to simulate failure
 		dbname   = "bookshoptest"
 		port     = "5432"
 	)
-	connStr := fmt.Sprintf("host = %s port= %s user= %s password= %s dbname= %s  sslmode=disable",
+	connStr := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
+
 	db, err := gorm.Open(postgres.Open(connStr), &gorm.Config{})
 	if err != nil {
-		panic(err)
+		fmt.Printf("Simulated connection failure: %v\n", err)
+		return nil // Return nil for *gorm.DB
 	}
-
 	return db
-
 }
